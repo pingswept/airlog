7d55e6523c8c1259ea900934cbf0adb1bd9ae0f7
Brandon Stafford <brandon@pingswept.org>
2021-08-29 21:43:19 +0100

    Make logger work, maybe

--- logger.py
+++ logger.py
@@ -5,11 +5,18 @@ import time
 import board
 import busio
 import adafruit_scd30
+import sqlite3
+import datetime as dt
+import pytz
 
 # SCD-30 has tempremental I2C with clock stretching, datasheet recommends
 # starting at 50KHz
 i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
 scd = adafruit_scd30.SCD30(i2c)
+
+tz = pytz.timezone('America/New_York')
+con = sqlite3.connect('airlog.db')
+cur = con.cursor()
 
 while True:
     # since the measurement interval is long (2+ seconds) we check for new data before reading
@@ -22,5 +29,9 @@ while True:
         print("")
         print("Waiting for new data...")
         print("")
+        db_write_cmd = 'INSERT INTO readings VALUES ("{0}", {1}, {2}, {3})'.format(dt.datetime.now(tz).strftime("%m/%d/%Y, %H:%M:%S"), scd.CO2, scd.temperature, scd.relative_humidity)
+        print(db_write_cmd)
+        cur.execute(db_write_cmd)
+        con.commit()
 
-    time.sleep(0.5)
+    time.sleep(15)
