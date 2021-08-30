214a95e13334852b42296d4ce69457f9eb371362
Brandon Stafford <brandon@pingswept.org>
2021-08-30 20:46:52 +0100

    Add chart.js, more data, and such

--- airlog.py
+++ airlog.py
@@ -1,20 +1,11 @@
 import time
 import board
 import busio
-import adafruit_scd30
 from flask import Flask, render_template
-
-i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
-scd = adafruit_scd30.SCD30(i2c)
 
 app = Flask(__name__)
 
 @app.route("/")
 def test_concentration():
     return render_template('index.htm')
-
-#while True:
-#    if scd.data_available:
-#        print("CO2: %d PPM" % scd.CO2)
-#    time.sleep(0.5)
 
