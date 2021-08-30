638b509a7b5dbff73d431e7c36c554067754feee
Brandon Stafford <brandon@pingswept.org>
2021-08-30 15:34:45 +0100

    Update script and add a day of data

--- airlog.py
+++ airlog.py
@@ -2,7 +2,7 @@ import time
 import board
 import busio
 import adafruit_scd30
-from flask import Flask
+from flask import Flask, render_template
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=50000)
 scd = adafruit_scd30.SCD30(i2c)
@@ -11,4 +11,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def test_concentration():
-    return "<p>CO2 concentration is %d PPM.</p>" % scd.CO2
+    return render_template('index.htm')
+
+#while True:
+#    if scd.data_available:
+#        print("CO2: %d PPM" % scd.CO2)
+#    time.sleep(0.5)
+
