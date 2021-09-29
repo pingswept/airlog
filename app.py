9e838ea81bc6ca20f39a74e97a3d2768de5ba63a
Brandon Stafford <brandon@pingswept.org>
2021-09-29 20:27:02 +0100

    More work on date range retrieval

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import datetime as dt
 import time
 import pandas as pd
+import requests
 import sqlite3
 from flask import Flask, render_template
 
@@ -7,15 +9,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def test_concentration():
-    con = sqlite3.connect("airlog.db")
-    df = pd.read_sql_query("SELECT * FROM readings", con)
+    delta = dt.timedelta(days=2)
+    addr = 'http://air1.nolop.org:5000/range/' + '2021-09-23' + '/' + dt.date.today().isoformat()
+    r = requests.get('http://air1.nolop.org:5000/range/2021-09-23/2021-09-25')
+    df = r.json()
 
-    con.close()
-
-    timestamps = df['date'].values.tolist()
-    data = df['co2'].values.tolist()
-
-    return render_template('index.htm', timestamps=timestamps, data=data)
+    return render_template('index.htm', timestamps=df['timestamps'], data=df['data'])
 
 @app.route("/range/<startdate>/<enddate>")
 def get_date_range(startdate, enddate):
